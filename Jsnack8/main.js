4457382ff3d7cf55d268aa86b242daa135c0abe1
wass01 <maatallawassim01@gmail.com>
2021-03-03 18:56:01 +0100

    TUTTI GLI ESERCIZI TERMINATI

--- Jsnack8/main.js
+++ Jsnack8/main.js
@@ -1,14 +1,16 @@
 // Chiedi un numero di 4 cifre all’utente
 // e calcola la somma di tutte le cifre che compongono il numero.
 
-var numero = parseInt(prompt("Inserisci un numero di 4 cifre:"));
-var n = [];
-
+var numero = prompt("Inserisci un numero di 4 cifre:");
+var somma = 0;
+var arrayNumero = Array.from(numero);
 
 if(numero <= 999 || numero >= 10000){
   console.log("Il numero non è da 4 cifre");
 } else {
-  n.push(numero);
-  var somma = n[0] + n[1] + n[2] + n[3];
-  console.log(somma);
+  for (var i = 0; i < arrayNumero.length; i++) {
+    somma += parseInt(arrayNumero[i]);
+  }
 }
+
+console.log(somma);
